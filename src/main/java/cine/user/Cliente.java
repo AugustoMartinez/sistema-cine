5e82576de7ee70dd14b45fda0f3be58e30878f83
Augusto Martinez <augustomartinezt5@gmail.com>
2023-06-13 15:14:44 -0300

    Crear clases

--- src/main/java/cine/user/Cliente.java
+++ src/main/java/cine/user/Cliente.java
@@ -1,4 +1,29 @@
 package cine.user;
 
-public class Cliente {
+import cine.cinelugar.Reserva;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Cliente extends Usuario{
+    List listaReserva = new ArrayList<Reserva>();
+    Map combos = new HashMap<String, Integer>();
+    public Cliente(String email, String password, String nombre, String apellido) {
+        super(email, password, nombre, apellido);
+    }
+
+    @Override
+    public void logeo() {
+
+    }
+
+    public void registrar(){
+
+    }
+
+    public void reservarFuncion(){
+
+    }
 }
--- src/main/java/cine/user/Usuario.java
+++ src/main/java/cine/user/Usuario.java
@@ -1,4 +1,57 @@
 package cine.user;
 
-public class Usuario {
+public abstract class Usuario {
+    private String email;
+    private String password;
+    private String nombre;
+    private String apellido;
+
+    public Usuario(String email, String password, String nombre, String apellido) {
+        this.email = email;
+        this.password = password;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public abstract void logeo();
+
+    @Override
+    public String toString() {
+        return "Informacion del usuario: { \n" +
+                "Nombre y apellido: '" + nombre + apellido + "\n" +
+                "Contraseña: " + password + "\n" +
+                "Email: '" + nombre + '\n' + '}';
+    }
 }
