7b9fcf0b7241388abca021e9c1845780c3893318
DiegoPineda <diegopineda819@gmail.com>
2023-06-17 14:07:45 -0300

    excepciones

--- src/main/java/cine/user/Usuario.java
+++ src/main/java/cine/user/Usuario.java
@@ -1,7 +1,9 @@
 package cine.user;
 
+import excepciones.CampoVacioException;
 import excepciones.EmailException;
 import excepciones.LetrasException;
+import excepciones.LongitudPasswordException;
 
 public abstract class Usuario {
     private String email;
@@ -20,9 +22,11 @@ public abstract class Usuario {
         return email;
     }
 
-    public void setEmail(String email) throws EmailException{
+    public void setEmail(String email) throws EmailException,CampoVacioException{
         if (!email.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")) {
             throw new EmailException("Email inválido");
+        }else if(email.isEmpty()){
+        throw new CampoVacioException("email");
         }else{
             this.email = email;
         }
@@ -32,7 +36,12 @@ public abstract class Usuario {
         return password;
     }
 
-    public void setPassword(String password) {
+    public void setPassword(String password) throws LongitudPasswordException,CampoVacioException {
+        if (password.length() < 8) {
+        throw new LongitudPasswordException();
+    }else if(password.isEmpty()){
+        throw new CampoVacioException("contraseña");
+    }
         this.password = password;
     }
 
@@ -40,9 +49,11 @@ public abstract class Usuario {
         return nombre;
     }
 
-    public void setNombre(String nombre) throws LetrasException{
+    public void setNombre(String nombre) throws LetrasException,CampoVacioException{
         if (!nombre.matches("[a-zA-Z]+")) {
             throw new LetrasException("El campo debe contener solo letras");
+        }else if(nombre.isEmpty()){
+        throw new CampoVacioException("nombre");
         }else{
             this.nombre = nombre;
         }
@@ -52,9 +63,11 @@ public abstract class Usuario {
         return apellido;
     }
 
-    public void setApellido(String apellido) throws LetrasException{
+    public void setApellido(String apellido) throws LetrasException,CampoVacioException{
         if (!apellido.matches("[a-zA-Z]+")) {
             throw new LetrasException("El campo debe contener solo letras");
+        }else if(nombre.isEmpty()){
+        throw new CampoVacioException(apellido);
         }else{
             this.apellido = apellido;
         }
